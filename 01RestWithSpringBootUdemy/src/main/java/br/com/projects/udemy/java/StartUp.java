5a0d99d205c41734fbf4a37f50a9a4b63c697933
dev <luiz.fariajr@outlook.com>
2020-02-28 20:38:41 -0300

    primeira api rest

---
+++ 01RestWithSpringBootUdemy/src/main/java/br/com/projects/udemy/java/StartUp.java
@@ -0,0 +1,12 @@
+package br.com.projects.udemy.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUp {
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(StartUp.class, args);
+	}
+}
