b4b12621a6cc92c36ad72029ee12b8880e073070
fariajr <luiz.fariajr@outlook.com>
2020-03-01 22:43:43 -0300

    progressao do curso

--- 01RestWithSpringBootUdemy/src/main/java/br/com/projects/udemy/java/StartUp.java
+++ 01RestWithSpringBootUdemy/src/main/java/br/com/projects/udemy/java/StartUp.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StartUp {
-	
-	public static void main(String[] args) throws Exception {
+
+	public static void main(String[] args) {
 		SpringApplication.run(StartUp.class, args);
 	}
 }
